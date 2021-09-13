425d7a5179612e9568f0c79531773e1e0e6411cc
우상현 <woosanghyun32@gmail.com>
2021-09-13 13:13:17 +0900

    binding images

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       binding = ActivityMainBinding.inflate(getLayoutInflater());
+       View view = binding.getRoot();
+       setContentView(view);
     }
 
     public void onClick(View view) {
+
+        ImageView imageView = findViewById(R.id.imageView);
+        binding.imageView.setImageResource(R.drawable.hi);
+        Toast.makeText(this,"이미지를 보여줍니다", Toast.LENGTH_LONG).show();
+
     }
 
 
